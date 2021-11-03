79f4d94c51e60e2cf7668f5432b7feb21a4a9727
Shrey Marwaha <shrey19334@iiitd.ac.in>
2021-11-04 03:36:41 +0530

    connected to infura api, deployed contract to rinkeby

--- IBC/Smart Contracts/test/inbox.test.js
+++ IBC/Smart Contracts/test/inbox.test.js
@@ -1,7 +1,8 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
-const Web3 = require("web3"); //constructor, for creating instances of web3
-const web3 = new Web3(ganache.provider());
+const Web3 = require("web3");
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require("../compile");
 /*** HOW TO TEST : npm run test */
 
@@ -26,14 +27,27 @@ describe("Car", () => {
 */
 let accounts, inbox;
 beforeEach(async () => {
-	// Get a list of all accounts
-	accounts = await web3.eth.getAccounts();
+    // Get a list of 10 ganache accounts
+    accounts = await web3.eth.getAccounts();
 
-	//Use one of rthose account to deploy the contract
-	inbox = await new web3.eth.Contract(JSON.parse(interface)).deploy({ data: bytecode, arguments: ["Hi There!"] }).send({ from: accounts[0], gas: "1000000" });
+    //Use one of 10 ganache account to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ["Hi There!"] })
+        .send({ from: accounts[0], gas: "1000000" });
 });
 describe("Inbox", () => {
-	it("deploys a contract", () => {
-		console.log(inbox);
-	});
+    it("deploys a contract", () => {
+        console.log(inbox);
+    });
+
+    it("has a default messge", async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, "Hi There!");
+    });
+
+    it("can change the message", async () => {
+        await inbox.methods.setMessage("Bye Shrey").send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, "Bye Shrey");
+    });
 });
