efa4326d63a0f63e9dd130dfd4cea123dada0844
Shrey Marwaha <shrey19334@iiitd.ac.in>
2021-11-21 14:23:53 +0530

    Nft backend complete
    NFT for color hex code.

--- IBC/Nft/src/contracts/Color.sol
+++ IBC/Nft/src/contracts/Color.sol
@@ -1,8 +1,18 @@
 pragma solidity 0.5.0;
-
-import "./ERC721.sol";
-contract Color is ERC721{
-    constructor() ERC721("Color", "COLOR") public{
-        
+import "./ERC721Full.sol";
+contract Color is ERC721Full{
+    string[] public colors;
+    mapping(string => bool)_colorExists;
+    constructor() ERC721Full("Color", "COLOR") public{    }
+    
+    function mint(string memory _color) public{
+        // require unique color
+        require(!_colorExists[_color]); //if color exits, trigger exception
+        //color - track it and add it
+        uint _id = colors.push(_color); //returns length
+        // call the mint function
+        _mint(msg.sender, _id); 
+        // Color - track it
+        _colorExists[_color] = true;
     }
-}
+ }
