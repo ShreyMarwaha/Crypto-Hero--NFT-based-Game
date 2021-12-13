a9a481b40db07da89c4df8dbc9acbcb40fda45de
Shrey Marwaha <shrey19334@iiitd.ac.in>
2021-12-13 13:55:46 +0530

    v1

--- IBC/Nft/src/components/App.js
+++ IBC/Nft/src/components/App.js
@@ -4,10 +4,24 @@ import "./App.css";
 import Color from "../abis/Color.json";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            account: "",
+            contract: null,
+            totalSupply: 0,
+            Heros: []
+        };
+    }
+
+    /**► Called on server side, as soon as component mounts */
     async componentWillMount() {
         await this.loadWeb3();
         await this.loadBlockchainData();
     }
+
+    /**▶ Checking for Metamask Installation for login */
     async loadWeb3() {
         //new way
         if (window.ethereum) {
@@ -25,6 +39,7 @@ class App extends Component {
             );
         }
     }
+
     async loadBlockchainData() {
         const web3 = window.web3;
         const accounts = await web3.eth.getAccounts();
@@ -41,105 +56,135 @@ class App extends Component {
             this.setState(totalSupply);
             //load colors
             for (var i = 0; i < totalSupply; i++) {
-                const color = await contract.methods.colors(i).call();
+                const hero = await contract.methods.allHero(i).call();
+                console.log(
+                    hero.color,
+                    hero.owner,
+                    hero.attack,
+                    hero.defence,
+                    hero.health
+                );
+                // const ownerAddr = await contract.methods.owner(i).call();
+                // console.log("from:", ownerAddr);
                 this.setState({
-                    colors: [...this.state.colors, color]
+                    Heros: [...this.state.Heros, hero]
                 });
-                console.log(this.state.colors);
             }
         } else {
             window.alert("Smart contract not deployed to detected network.");
         }
     }
-    mint = (color) => {
+    async mint(color) {
         console.log(color);
         this.state.contract.methods
             .mint(color)
-            .send({ from: this.state.account })
-            .once("receipt", (receipt) => {
-                this.setState({
-                    colors: [...this.state.colors, color]
-                });
-            });
-    };
-    constructor(props) {
-        super(props);
-        this.state = {
-            account: "",
-            contract: null,
-            totalSupply: 0,
-            colors: []
-        };
+            .send({ from: this.state.account });
+
+        const last_idx =
+            (await this.state.contract.methods.totalSupply().call()) - 1;
+        const hero = await this.state.contract.methods.allHero(last_idx).call();
+        this.setState({ Heros: [...this.state.Heros, hero] });
     }
+    async fight(e) {
+        e.preventDefault();
+        const hero = await this.state.contract.methods.allHero(0).call();
+        console.log("in fight");
+        // console.log(await this.state.contract.methods.allHero(0).call());
+        // const my = Math.random(this.state.Heros.length);
+        // const other = Math.random(this.state.Heros.length);
+        // my = this.state.Heros[my];
+        // other = this.state.Heros[other];
+        // console.log(my.color, my.health);
+        // console.log(other.color, other.health);
+    }
+
     render() {
-        return (
-            <div>
-                <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-                    <a
-                        className="navbar-brand col-sm-3 col-md-2 mr-0"
-                        href="http://www.google.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Color Tokens
-                    </a>
-                    <ul className="navbar-nav px-3">
-                        <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-                            <small className="text-white">
-                                <span id="account">
-                                    Account: {this.state.account}
-                                </span>
-                            </small>
-                        </li>
-                    </ul>
-                </nav>
-                <div className="container-fluid mt-5">
-                    <div className="row">
-                        <main
-                            role="main"
-                            className="col-lg-12 d-flex text-center"
+        if (this.state.account) {
+            return (
+                <div>
+                    <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+                        <a
+                            className="navbar-brand col-sm-3 col-md-2 mr-0"
+                            href="http://www.google.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
                         >
-                            <div className="content mr-auto ml-auto">
-                                <h1>Issue Token</h1>
-                                <form
-                                    onSubmit={(event) => {
-                                        event.preventDefault();
-                                        const color = this.color.value;
-                                        this.mint(color);
-                                    }}
-                                >
-                                    <input
-                                        type="text"
-                                        className="fomr-control mb-1"
-                                        placeholder="eg. #FFFFFF"
-                                        ref={(input) => (this.color = input)}
-                                    />
-                                    <input
-                                        type="submit"
-                                        className="btn btn-block btn-primary"
-                                        value="MINT"
-                                    />
-                                </form>
-                            </div>
-                        </main>
-                    </div>
-                    <hr />
-                    <div className="row text-center">
-                        {this.state.colors.map((color, key) => {
-                            return (
-                                <div key={key} className="col-md-3 mb-3">
-                                    <div
-                                        className="token"
-                                        style={{ backgroundColor: color }}
-                                    ></div>
-                                    <div>{color}</div>
+                            An AF Tea
+                        </a>
+                        <ul className="navbar-nav px-3">
+                            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+                                <small className="text-white">
+                                    <span id="account">
+                                        Account: {this.state.account}
+                                    </span>
+                                </small>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <div className="container-fluid mt-5">
+                        <div className="row">
+                            <main
+                                role="main"
+                                className="col-lg-12 d-flex text-center"
+                            >
+                                <div className="content mr-auto ml-auto">
+                                    <h1>Issue Hero Token</h1>
+                                    <form
+                                        onSubmit={(event) => {
+                                            event.preventDefault();
+                                            const color = this.color.value;
+                                            this.mint(color);
+                                        }}
+                                    >
+                                        <input
+                                            type="text"
+                                            className="fomr-control mb-1"
+                                            placeholder="eg. #FFFFFF"
+                                            ref={(input) =>
+                                                (this.color = input)
+                                            }
+                                        />
+                                        <input
+                                            type="submit"
+                                            className="btn btn-block btn-primary"
+                                            value="MINT"
+                                        />
+                                    </form>
                                 </div>
-                            );
-                        })}
+                            </main>
+                        </div>
+                        <hr />
+                        <div className="row text-center">
+                            {this.state.Heros.map((hero, key) => {
+                                return (
+                                    <div key={key} className="col-md-3 mb-3">
+                                        <div
+                                            className="token"
+                                            style={{
+                                                backgroundColor: hero.color
+                                            }}
+                                        ></div>
+                                        <div>{hero.color}</div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            className="btn btn-block btn-primary"
+                            onClick={this.fight}
+                        >
+                            Fight!
+                        </button>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+            return <div>Login & verify your Metamask account.</div>;
+        }
     }
 }
 
--- IBC/Nft/src/contracts/Color.sol
+++ IBC/Nft/src/contracts/Color.sol
@@ -1,18 +1,76 @@
 pragma solidity 0.5.0;
 import "./ERC721Full.sol";
 contract Color is ERC721Full{
-    string[] public colors;
-    mapping(string => bool)_colorExists;
-    constructor() ERC721Full("Color", "COLOR") public{    }
-        
-    function mint(string memory _color) public{
+
+    struct Hero{
+        string color;
+        address owner;
+        uint health;
+        uint attack;
+        uint defence;
+    }
+    Hero[] public allHero;
+    // string[] public colors;
+    // address[] public owner;
+    mapping(string => bool) colorExists;
+    
+
+    constructor() ERC721Full("Color", "COLOR") public{}
+
+    /**Returns random uint */
+    uint randNonce = 0;
+ 
+    /**Returns a random number */
+    function random(uint _modulus) internal returns(uint)
+    {
+        randNonce++; 
+        return uint(keccak256(abi.encodePacked(now,msg.sender,randNonce))) % _modulus;
+    }
+
+    /**Minting the new Hero */
+    function mint(string memory color) public{
         // require unique color
-        require(!_colorExists[_color]); //if color exits, trigger exception
-        //color - track it & add it
-        uint _id = colors.push(_color); //returns length
-        // call the mint function
-        _mint(msg.sender, _id); 
-        // Color - track it
-        _colorExists[_color] = true;
+        require(!colorExists[color]); //if color exits, trigger exception
+
+        Hero memory newHero = Hero(color, msg.sender, random(100), random(100), random(100), 0);
+        uint id = allHero.push(newHero);//returns length
+        // colors.push(color); 
+        _mint(msg.sender, id); 
+        colorExists[color] = true;
+
     }
  }
+
+ /**
+ 1. To create new blockchain
+ sudo truffle migrate --reset
+
+ 2. Open truffle console
+ truffle console
+
+ 3. Create contract
+ contract = await Color.deployed()
+
+ 4. Call any methods
+ e.g. contract.colors(0)
+
+  */
+
+  /**
+  TODO
+  1. health, defence, attack comes as BigNumber in console : convert to normal integer
+  2. 
+  3. Only show tokens owned by metamask owner
+  4. a seperate page / on hover token show stats(attack, health, defence)
+  5. fight page + on Navbar
+  6. random hex generator
+  7. hero pics instead of cicles
+  8.
+
+   */
+
+   /**
+   BUGS
+1. while adding new token, new token is visible of prev color, but corrects after reloading.
+2. adding token of anything gets added to chain: make a checker for only valid hex color
+    */
